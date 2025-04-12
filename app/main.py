6989d1c84797a9c50099991adfc46039739aa18d
Soumya Maheshwari <soumyamaheshwari1234@gmail.com>
2025-04-12 11:23:51 +0530

    Updated

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
+import os
 
 app = Flask(__name__)
 
-@app.route('/transactions', methods=['GET'])
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/transactions", methods=["GET"])
 def get_transactions():
     return jsonify([
         {"id": 1, "amount": 200, "currency": "USD", "type": "deposit"},
-        {"id": 2, "amount": -50, "currency": "USD", "type": "withdrawal"}
+        {"id": 2, "amount": -50, "currency": "USD", "type": "withdrawal"},
+        {"id": 3, "amount": 1200, "currency": "INR", "type": "deposit"}
     ])
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5050))
+    app.run(debug=True, host="0.0.0.0", port=port)
